ef95df7a9f28b350ddc42847688e902ab55e6291
M0ngi <saidanemongi@gmail.com>
2022-12-26 02:43:04 +0100

    Extract 2 more decorators

--- gef.py
+++ gef.py
@@ -98,7 +98,7 @@ from gefSrc.util.helper import *
 from gefSrc.util.display_helper import *
 from gefSrc.util.register_feature import register
 from gefSrc.util.http_helper import http_get, update_gef
-from gefSrc.util.decorators import only_if_gdb_running, only_if_gdb_target_local
+from gefSrc.util.decorators import *
 from gefSrc.managers import GefManager, GefUiManager
 from gefSrc.config import Config, GefSetting
 from gefSrc.commands import GenericCommand
@@ -148,43 +148,6 @@ def reset() -> None:
 #
 # Decorators
 #
-
-
-def deprecated(solution: str = "") -> Callable:
-    """Decorator to add a warning when a command is obsolete and will be removed."""
-    def decorator(f: Callable) -> Callable:
-        @functools.wraps(f)
-        def wrapper(*args: Any, **kwargs: Any) -> Any:
-            caller = inspect.stack()[1]
-            caller_file = pathlib.Path(caller.filename)
-            caller_loc = caller.lineno
-            msg = f"{caller_file.name}:L{caller_loc} '{f.__name__}' is deprecated and will be removed in a feature release. "
-            if not gef:
-                print(msg)
-            elif RunTimeGlobals.gef.config["RunTimeGlobals.gef.show_deprecation_warnings"] is True:
-                if solution:
-                    msg += solution
-                warn(msg)
-            return f(*args, **kwargs)
-
-        if not wrapper.__doc__:
-            wrapper.__doc__ = ""
-        wrapper.__doc__ += f"\r\n`{f.__name__}` is **DEPRECATED** and will be removed in the future.\r\n{solution}"
-        return wrapper
-    return decorator
-
-
-def experimental_feature(f: Callable) -> Callable:
-    """Decorator to add a warning when a feature is experimental."""
-
-    @functools.wraps(f)
-    def wrapper(*args: Any, **kwargs: Any) -> Any:
-        warn("This feature is under development, expect bugs and unstability...")
-        return f(*args, **kwargs)
-
-    return wrapper
-
-
 def only_if_events_supported(event_type: str) -> Callable:
     """Checks if GDB supports events without crashing."""
     def wrap(f: Callable) -> Callable:
--- gefSrc/util/decorators.py
+++ gefSrc/util/decorators.py
@@ -1,8 +1,11 @@
 from gefSrc.util.helper import is_alive
 from gefSrc.util.display_helper import warn
 from gefSrc.util.helper import is_remote_debug
+from gefSrc.globals import RunTimeGlobals
 from typing import (Callable, Any)
 import functools
+import pathlib
+import inspect
 
 
 def only_if_gdb_running(f: Callable) -> Callable:
@@ -27,5 +30,40 @@ def only_if_gdb_target_local(f: Callable) -> Callable:
             return f(*args, **kwargs)
         else:
             warn("This command cannot work for remote sessions.")
+
+    return wrapper
+
+
+def deprecated(solution: str = "") -> Callable:
+    """Decorator to add a warning when a command is obsolete and will be removed."""
+    def decorator(f: Callable) -> Callable:
+        @functools.wraps(f)
+        def wrapper(*args: Any, **kwargs: Any) -> Any:
+            caller = inspect.stack()[1]
+            caller_file = pathlib.Path(caller.filename)
+            caller_loc = caller.lineno
+            msg = f"{caller_file.name}:L{caller_loc} '{f.__name__}' is deprecated and will be removed in a feature release. "
+            if not gef:
+                print(msg)
+            elif RunTimeGlobals.gef.config["RunTimeGlobals.gef.show_deprecation_warnings"] is True:
+                if solution:
+                    msg += solution
+                warn(msg)
+            return f(*args, **kwargs)
+
+        if not wrapper.__doc__:
+            wrapper.__doc__ = ""
+        wrapper.__doc__ += f"\r\n`{f.__name__}` is **DEPRECATED** and will be removed in the future.\r\n{solution}"
+        return wrapper
+    return decorator
+
+
+def experimental_feature(f: Callable) -> Callable:
+    """Decorator to add a warning when a feature is experimental."""
+
+    @functools.wraps(f)
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        warn("This feature is under development, expect bugs and unstability...")
+        return f(*args, **kwargs)
 
     return wrapper
